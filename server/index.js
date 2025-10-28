85f95f53c092362129b62bedf8e0a936c0a1879e
Joseph Nyarandi <josephokero074@gmail.com>
2025-10-28 03:01:58 +0300

    fix(server): update cors configuration to allow client origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,25 @@ const mpesaCallbackRoutes = require('./mpesaCallback');
 const app = express();
 
 
-app.use(cors());
+const allowedOrigins = [
+  'https://sureboda.com',
+  'http://localhost:3000',
+  'http://localhost:5000'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+// Explicitly handle preflight requests for all routes
+app.options('*', cors());
 app.use(express.json());
 
 // M-Pesa API routes
